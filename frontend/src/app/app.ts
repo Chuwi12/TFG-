15539bada01e3329d795a82d5c8fd2fc3ebd50ad
jvrbnt <jvr.bnt.rnz@gmail.com>
2026-03-23 01:44:38 +0100

    Añadida la logica del chat usando Señales y pulido de la UI

--- frontend/src/app/app.ts
+++ frontend/src/app/app.ts
@@ -5,8 +5,24 @@ import { RouterOutlet } from '@angular/router';
   selector: 'app-root',
   imports: [RouterOutlet],
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  styleUrl: './app.css',
 })
 export class App {
-  protected readonly title = signal('frontend');
+  protected readonly title = signal('Financial Termianl');
+
+  // serhio voy a crear un señal con la lista d emensajaes incial
+  protected readonly messages = signal([
+    { text: 'SYSTEM READY. Authorized access only.', sender: 'system' },
+    { text: 'Waiting for financial query...', sender: 'system' },
+  ]);
+
+  // funciona para aadirm los mensajes del usuario
+  addMessage(input: HTMLInputElement) {
+    const value = input.value.trim();
+    if (value) {
+      // actualizar señal
+      this.messages.update((prev) => [...prev, { text: value, sender: 'user' }]);
+      input.value = '';
+    }
+  }
 }
